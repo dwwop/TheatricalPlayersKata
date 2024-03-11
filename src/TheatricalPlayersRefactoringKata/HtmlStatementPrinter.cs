d67b11a805d5d866c09ea17e70bd071756a92320
Dávid Laurovič <xlaurov@fi.muni.cz>
2024-03-11 17:05:37 +0100

    added html prefix and sufix

--- src/TheatricalPlayersRefactoringKata/HtmlStatementPrinter.cs
+++ src/TheatricalPlayersRefactoringKata/HtmlStatementPrinter.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace TheatricalPlayersRefactoringKata;
 
 public class HtmlStatementPrinter : StatementPrinter
 {
-    
-    
+
+    public override string Print(Invoice invoice, Dictionary<string, Play> plays)
+    {
+        return "<!DOCTYPE html>\n<html>\n<body>" + base.Print(invoice, plays) + "</body>\n</html>";
+
+    }
+
+
+
     protected override string GetPlayLine(Play play, Performance perf, CultureInfo cultureInfo, int thisAmount)
     {
         return "<p>" + base.GetPlayLine(play, perf, cultureInfo, thisAmount) + "</p>";
